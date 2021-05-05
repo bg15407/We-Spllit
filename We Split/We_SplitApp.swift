8111562d3c3db2d2d05dbb48ab0a09338cd0c097
Gayan Kalinga <bg15407@gmail.com>
2021-05-05 17:23:29 +0530

    Initial Commit

---
+++ We Split/We_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  We_SplitApp.swift
+//  We Split
+//
+//  Created by Gayan Kalinga on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct We_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
