f45f699e1b799cf2b1b36a36f06b7c6f20e261ef
Gayan Kalinga <bg15407@gmail.com>
2021-05-13 18:19:47 +0530

    Inital Commit with all the challenges completed

--- We Split/ContentView.swift
+++ We Split/ContentView.swift
@@ -8,9 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var billAmount = ""
+    @State private var noOfPeople = "2"
+    @State private var selectedTip = 0
+    
+    let tipPercentages = [0, 5, 10, 15, 20]
+    
+    var totalBill: Double {
+        let amount = Double(billAmount) ?? 0.0
+        let tip = Double(tipPercentages[selectedTip])
+        print(tipPercentages[selectedTip])
+        let total = amount + amount * tip / 100.0
+        return total
+    }
+    
+    var amountPerPerson: Double{
+        let sharedBy = Double(noOfPeople) ?? 2.0
+        return totalBill / sharedBy
+    }
+    
+    func saveDetail(){
+        print("Detail Saved")
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form{
+                Section(header: Text("Total Bill Amount")){
+                    TextField("Bill Amount", text: $billAmount)
+                        .keyboardType(.numberPad)
+                }
+                
+                Section(header: Text("No of Peoples")){
+                    TextField("No of People", text: $noOfPeople)
+                        .keyboardType(.numberPad)
+  
+                }
+                
+                Section(header: Text("Tip Percentage")){
+                    Picker("Tip Percentage", selection: $selectedTip){
+                        ForEach( 0 ..< tipPercentages.count){ item in
+                              Text("\(tipPercentages[item])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Total Per Person")){
+                    Text("\(amountPerPerson, specifier: "%.2f")")
+                }
+                Section(header: Text("Total Bill")){
+                    Text("\(totalBill, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("We Split", displayMode: .automatic)
+            .toolbar{
+                ToolbarItemGroup(placement: .navigationBarTrailing){
+                    Button{
+                        saveDetail()
+                    }label: {
+                        Text("Save")
+                    }
+                }
+            }
+        }
+        .accentColor(.green)
     }
 }
 
